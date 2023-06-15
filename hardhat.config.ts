4ed077edf12d764a3097b9732d5552aefc37d723
roy <royfangs@gmail.com>
2023-06-15 20:15:28 +0800

    feat: update package

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,17 +1,20 @@
 import '@nomicfoundation/hardhat-toolbox';
+import '@nomicfoundation/hardhat-chai-matchers';
+import '@nomiclabs/hardhat-etherscan';
 import 'hardhat-abi-exporter';
+import '@openzeppelin/hardhat-upgrades';
 import dotenv from 'dotenv';
 
 // types
-import type { HardhatUserConfig } from 'hardhat/config';
+// import type { HardhatUserConfig } from 'hardhat/config';
 
 dotenv.config();
 
-const config: HardhatUserConfig = {
+const config = {
   solidity: {
     compilers: [
       {
-        version: '0.8.7',
+        version: '0.8.8',
         settings: {
           optimizer: {
             enabled: true,
@@ -23,6 +26,28 @@ const config: HardhatUserConfig = {
         },
       },
     ],
+  },
+  networks: {
+    mainnet: {
+      url: `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY_MAINNET}`,
+      accounts: [
+        process.env.ACCOUNT_PRIVATE_KEY_OWNER_MAINNET!,
+        process.env.ACCOUNT_PRIVATE_KEY_USER_MAINNET!,
+      ].filter(Boolean),
+    },
+    goerli: {
+      url: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY_GOERLI}`,
+      accounts: [
+        process.env.ACCOUNT_PRIVATE_KEY_OWNER_GOERLI!,
+        process.env.ACCOUNT_PRIVATE_KEY_USER_GOERLI!,
+      ].filter(Boolean),
+    },
+    hardhat: {
+      initialBaseFeePerGas: 0,
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
   },
   abiExporter: {
     path: 'abi',
